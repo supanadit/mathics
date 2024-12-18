cab74785053b54eea6d3b4026f30aa86e4e4f7b8
Supan Adit Pratama <email@supanadit.com>
2024-12-19 00:00:47 +0700

    feat: update correct file name

--- cmd/main.go
+++ cmd/main.go
@@ -14,12 +14,12 @@ func main() {
 
 	//fmt.Println(mathics.GCDT(2625, 1000))
 	//fmt.Println(mathics.GCDT(2664, 999))
-	//fmt.Println(mathics.GCDT(336, 360))
-	//fmt.Println(mathics.GCDM([]int{5, 10, 15, 25}))
+	fmt.Println(mathics.GCDT(12, 20))
+	fmt.Println(mathics.GCDM([]int{1, 2, 3, 4, 6, 12}))
 	//fmt.Println(mathics.LCMM([]int{100, 90, 80, 7}))
-	fmt.Println(mathics.LCMM([]int{1, 2, 3, 5, 6, 10, 15, 30}))
-	fmt.Println(mathics.LCMT(3, 15))
-	fmt.Println(mathics.LCMT(5, 7))
-	fmt.Println(mathics.LCMT(10, 11))
+	// fmt.Println(mathics.LCMM([]int{1, 2, 3, 5, 6, 10, 15, 30}))
+	// fmt.Println(mathics.LCMT(3, 15))
+	// fmt.Println(mathics.LCMT(5, 7))
+	// fmt.Println(mathics.LCMT(10, 11))
 	//fmt.Println(mathics.LCMT(5, 15))
 }
